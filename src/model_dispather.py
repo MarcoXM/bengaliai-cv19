181bb2d9743463e8d1c0937f65a54b00decc1ab9
MarcoXM <xwang423@fordham.edu>
2020-02-24 01:03:52 -0500

    save before add new features

--- src/model_dispather.py
+++ src/model_dispather.py
@@ -5,7 +5,8 @@ MODEL_DISPATCHER = {
     "resnet152":models.ResNet152,
     "resnet34":models.ResNet34,
     'se_net':models.SResnet,
-    'pnasnet':models.Pnasnet
+    'pnasnet':models.Pnasnet,
+    'inresnet':models.Inresnet
 
 }
 
