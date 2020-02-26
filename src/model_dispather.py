a5b8a63a498e03c110592e363fe1aa3f97f3f010
MarcoXM <xwang423@fordham.edu>
2020-02-25 22:54:02 -0500

    add inception v4

--- src/model_dispather.py
+++ src/model_dispather.py
@@ -3,12 +3,13 @@ import pretrainedmodels
 
 MODEL_DISPATCHER = {
     "resnet152":models.ResNet152,
-    "resnet34":models.ResNet34,
-    'se_net':models.SResnet,
-    'pnasnet':models.Pnasnet,
-    'inresnet':models.Inresnet,
-    'polynet':models.PolyNet
-
+    "resnet34":models.ResNet34, # 128
+    'se_net':models.SResnet, # 128
+    'pnasnet':models.Pnasnet, # 128
+    'inresnet':models.Inresnet, # 128
+    'polynet':models.PolyNet, # 128
+    'senet':models.SeNet, # 128
+    'icnetvf':models.IcNetv4, # 128
 }
 
 if __name__ =="__main__":
--- src/models.py
+++ src/models.py
@@ -167,3 +167,60 @@ class PolyNet(nn.Module):
         l2 = self.l2(x)
 
         return l0,l1,l2 
+
+
+class SeNet(nn.Module):
+    def __init__(self,pretrain = True):
+        super(SeNet,self).__init__()
+        if pretrain:
+            self.model = pretrainedmodels.__dict__['senet154'](pretrained = 'imagenet')
+        else:
+            self.model = pretrainedmodels.__dict__['senet154'](pretrained = None)
+
+        self.l0 = nn.Linear(2048,168)
+        self.l1 = nn.Linear(2048,11)
+        self.l2 = nn.Linear(2048,7)
+        self.a = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.metrics_keys = [
+            'loss', 'loss_grapheme', 'loss_vowel', 'loss_consonant',
+            'acc_grapheme', 'acc_vowel', 'acc_consonant']
+
+    def forward(self,x):
+        b = x.size(0)
+        x = self.model.features(x)
+        x = self.a(x).view(b,-1)
+
+        l0 = self.l0(x)
+        l1 = self.l1(x)
+        l2 = self.l2(x)
+
+        return l0,l1,l2 
+        
+        
+        
+class IcNetv4(nn.Module):
+    def __init__(self,pretrain = True):
+        super(IcNetv4,self).__init__()
+        if pretrain:
+            self.model = pretrainedmodels.__dict__['inceptionv4'](pretrained = 'imagenet')
+        else:
+            self.model = pretrainedmodels.__dict__['inceptionv4'](pretrained = None)
+
+        self.l0 = nn.Linear(1536,168)
+        self.l1 = nn.Linear(1536,11)
+        self.l2 = nn.Linear(1536,7)
+        self.a = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.metrics_keys = [
+            'loss', 'loss_grapheme', 'loss_vowel', 'loss_consonant',
+            'acc_grapheme', 'acc_vowel', 'acc_consonant']
+
+    def forward(self,x):
+        b = x.size(0)
+        x = self.model.features(x)
+        x = self.a(x).view(b,-1)
+
+        l0 = self.l0(x)
+        l1 = self.l1(x)
+        l2 = self.l2(x)
+
+        return l0,l1,l2 
